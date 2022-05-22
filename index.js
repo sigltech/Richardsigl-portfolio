0bcad2b8764ca201b6745cc21cd68dc36309ef92
sigltech <101203107+sigltech@users.noreply.github.com>
2022-05-23 00:23:45 +0100

    smooth scrolling added to projects with formatted and styled cards

--- index.js
+++ index.js
@@ -21,3 +21,12 @@ document.addEventListener('DOMContentLoaded', () => {
   
   });
 
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
